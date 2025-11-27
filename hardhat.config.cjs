360c9fa1d0621049410383d0781fce12309ced36
zealair12 <okechukwuzeal@gmail.com>
2025-11-27 13:23:54 -0600

    Create test cases

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,9 +1,9 @@
 const { mainnet, polygonAmoy } = require("@reown/appkit/networks");
 
 require("@nomiclabs/hardhat-waffle");
-const fs = required('fs')
-const privateKey = fs.readFileSync(".secret").toString().trim()()
-const projectId = '3423df6b2f7f46659b77b598e4398bdc'
+const fs = required("fs");
+const privateKey = fs.readFileSync(".secret").toString().trim()();
+const projectId = "3423df6b2f7f46659b77b598e4398bdc";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -15,19 +15,20 @@ module.exports = {
     polygonAmoy: {
       url: "https://polygon-amoy.infura.io/v3/${projectId}",
       accounts: [privateKey],
-      chainId: 80002
+      chainId: 80002,
     },
     mainnet: {
-      url: 'https://polygon-mainnet.infura.io/v3/${projectId}',
+      url: "https://polygon-mainnet.infura.io/v3/${projectId}",
       accounts: [privateKey],
-      chainId: 137
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+      chainId: 137,
+    },
+    solidity: {
+      version: "0.8.4",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
       },
     },
   },
